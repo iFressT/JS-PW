8c6f7bd3575c7f2367a1ae62c272a6f276427400
iFressT <hafizhjihaad@gmail.com>
2023-01-17 22:17:30 +0700

    First test

--- Playwright/tests/UIBasictest.spec.js
+++ Playwright/tests/UIBasictest.spec.js
@@ -1,8 +1,18 @@
-const {test} = require('@playwright/test');
+const {test, webkit} = require('@playwright/test');
 
 
-test ('Playwright first test',async ()=>{
+test ('Playwright first test',async ({browser})=>{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://www.bilibili.tv/id");
 
 
+
+
+
+});
+
+test.only ('Playwright first tests',async ({page})=>{
+    await page.goto("https://www.google.com/");
 
 });
